f9bdafcc9059b27c6de79b873a8ac16352f1bd07
simon <s2124363@student-dhbw-mannheim.de>
2023-06-15 18:11:47 +0200

    Login Register Update 1.4

--- app/src/main/java/com/example/mitfahrapp/RegisterActivity.java
+++ app/src/main/java/com/example/mitfahrapp/RegisterActivity.java
@@ -16,7 +16,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
