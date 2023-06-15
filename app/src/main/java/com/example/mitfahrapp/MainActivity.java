06dc1ffce09305c4ea7abe8b4d0489a7d54a0d35
simon <s212363@student.dhbw-mannheim.de>
2023-06-15 18:16:01 +0200

    Login Register Update 1.4

--- app/src/main/java/com/example/mitfahrapp/MainActivity.java
+++ app/src/main/java/com/example/mitfahrapp/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+
     }
 
     @Override
