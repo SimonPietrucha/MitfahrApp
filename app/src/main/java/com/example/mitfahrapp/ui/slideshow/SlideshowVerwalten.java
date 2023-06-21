825a4e11a391d423c37d7c38ad2bba66dbb8ee7f
martinm04 <martin.maier02@web.de>
2023-06-21 21:16:44 +0200

    added page for viewing and cancel rides

--- app/src/main/java/com/example/mitfahrapp/ui/slideshow/SlideshowVerwalten.java
+++ app/src/main/java/com/example/mitfahrapp/ui/slideshow/SlideshowVerwalten.java
@@ -4,15 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.mitfahrapp.databinding.FragmentSlideshowBinding;
 
-public class SlideshowFragment extends Fragment {
+public class SlideshowVerwalten extends Fragment {
 
     private FragmentSlideshowBinding binding;
 
